a59a113f72ce5326cb2a18ea3b78a00791f839c7
aleafs <zhangxc83@gmail.com>
2012-07-18 10:07:50 +0800

    Add Makefile

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 
 var Handle = require('./client');
 console.log(Handle);
+
+var Util    = require('util');
+
+var Master  = require('node-cluster').Master({
+  'pidfile' : __dirname + '/bench.pid',
+});
+
+Master.on('giveup', function (name, fatals) {
+  //XXX: alert
+  console.log(Util.format('Master giveup to restart %s process after %d times.', name, fatals));
+});
+
