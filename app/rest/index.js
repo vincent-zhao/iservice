0c03df94e6758638d18f7cb018dfc32deb9a97f4
aleafs <zhangxc83@gmail.com>
2012-07-24 09:05:34 +0800

    Upgrade pm to 0.9.1

--- app/rest/index.js
+++ app/rest/index.js
@@ -1,6 +1,14 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
 
+/**
+ * @监听列表
+ *
+ * key => [ response1, response2, ... ]
+ */
+var __rest_watchers = {};
+
 var http = require('http').createServer(function (req, res) {
+  console.log(req.url);
   res.end('hello world');
 });
 
