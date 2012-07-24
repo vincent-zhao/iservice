90749bf316d9417ff5a8518e5d5243740cafb518
aleafs <zhangxc83@gmail.com>
2012-07-24 11:17:47 +0800

    add emit delay test for watcher

--- test/unit/watcher.test.js
+++ test/unit/watcher.test.js
@@ -57,4 +57,25 @@ describe('watcher test', function () {
   });
   /* }}} */
 
+  /* {{{ should_watcher_emit_delay_works_fine() */
+  it('should_watcher_emit_delay_works_fine', function (done) {
+    var _me = watcher.create(5);
+
+    var res = [];
+    _me.push(function (data) {
+      res.push(data);
+      if (res.length === 2) {
+        res.should.eql([456, 789]);
+        done();
+      }
+    });
+
+    _me.emit(123);
+    _me.emit(456);
+    setTimeout(function () {
+      _me.emit(789);
+    }, 6);
+  });
+  /* }}} */
+
 });
