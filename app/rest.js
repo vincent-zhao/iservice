e03ea40f038d6252b656fb17c73d1e42b227f83b
aleafs <zhangxc83@gmail.com>
2012-08-08 21:31:54 +0800

    Add global objects register

--- app/rest.js
+++ app/rest.js
@@ -2,12 +2,29 @@
 
 "use strict";
 
-var http = require(__dirname + '/common/server.js').create({
-  'header_prefix'   : 'x-app-',
-    'control_root'  : __dirname + '/rest'
+var Factory = require('shark').factory;
+Factory.cleanAll();
+
+/**
+ * @ global config object
+ */
+var config  = require('shark').config.create(__dirname + '/../etc/rest.ini');
+Factory.setConfig('rest', config);
+
+['mysql', 'log', 'zookeeper'].forEach(function (type) {
+  var o = config.find(type);
+  var p = '#' + type + '/';
+  for (var i in o) {
+    Factory.setObject(p + i, o[i]);
+  }
+});
+
+var server  = require(__dirname + '/common/server.js').create({
+  'header_prefix' : 'x-app-',
+  'control_root'  : __dirname + '/rest'
 });
 
 require('pm').createWorker().ready(function (socket) {
-  http.emit('connection', socket);
+  server.emit('connection', socket);
 });
 
