e07f2e2acbce15b2b0783012e7907e3e2aa3f23c
aleafs <zhangxc83@gmail.com>
2012-08-08 18:16:35 +0800

    Add unittest for server.js

--- test/unit/server.test.js
+++ test/unit/server.test.js
@@ -13,10 +13,29 @@ describe('http server', function () {
 
   /* {{{ should_server_suite_works_fine() */
   it('should_server_suite_works_fine', function (done) {
-    urllib.request('http://localhost:33751', {}, function (error, data) {
+
+    var req = {
+      'type' : 'POST',
+    'data'   : {
+      'a' : 'b', 'cd' : 1
+    },
+    'headers' : {
+      'x-test-a1' : '12345',
+    'x-testa2'    : 'lalala',
+    'x-real-ip'   : '1.1.1.2',
+    }
+    };
+    urllib.request('http:/' + '/localhost:33751/default/hello', req, function (error, data) {
       should.ok(!error);
+      data  = JSON.parse(data.toString());
+      data.should.have.keys('time', 'url', 'data', 'info');
+      data.should.have.property('data', 'a=b&cd=1');
+      JSON.stringify(data.info).should.eql(JSON.stringify({
+        'a1'    : '12345',
+        'ipaddr': '1.1.1.2'
+      }));
       done();
-      });
+    });
   });
   /* }}} */
 
