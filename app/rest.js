9842484f1afc0ac99259b9448296f2aee2993bbf
aleafs <zhangxc83@gmail.com>
2012-08-17 20:55:42 +0800

    Add Zookeeper register

--- app/rest.js
+++ app/rest.js
@@ -14,26 +14,35 @@ Factory.cleanAll();
 var config  = Shark.config.create(__dirname + '/../etc/rest.ini');
 Factory.setConfig('rest', config);
 
-var _confs  = config.find('mysql');
-for (var i in _confs) {
-  Factory.setMysql(i, _confs[i]);
-}
-
-var _confs  = config.find('log');
-for (var i in _confs) {
-  Factory.setLog(i, _confs[i]);
-}
-
-var _confs  = config.find('zookeeper');
-for (var i in _confs) {
-  Factory.setObject('zookeeper#' + i, Storage.create(_confs[i]));
-}
-
 Shark.setExceptionLogger(config.get('log:error'));
 process.on('uncaughtException', function (e) {
   Shark.logException(e);
   process.exit(1);
 });
+
+/**
+ * @ global mysql objects
+ */
+var _confs  = config.find('mysql');
+for (var i in _confs) {
+  Factory.setMysql(i, _confs[i]);
+}
+
+/**
+ * @ global log objects
+ */
+var _confs  = config.find('log');
+for (var i in _confs) {
+  Factory.setLog(i, _confs[i]);
+}
+
+/**
+ * @ global zookeeper objects
+ */
+var _confs  = config.find('zookeeper');
+for (var i in _confs) {
+  Factory.setObject('#zookeeper/' + i, Storage.create(_confs[i]));
+}
 
 var server  = require(__dirname + '/common/server.js').create({
   'header_prefix' : 'x-app-',
